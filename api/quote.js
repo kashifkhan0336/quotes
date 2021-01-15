0b3760494f881342ae6413892772e34c14c47e2e
kashifkhan0336 <kashifkhan0336@gmail.com>
2021-01-14 17:33:42 -0800

    sim

--- api/quote.js
+++ api/quote.js
@@ -2,13 +2,5 @@ var fs = require("fs");
 var path = require("path");
 
 module.exports = (req, res) => {
-  var obj;
-  fs.readFile(path.join(__dirname, "_files", "quotes.json"), "utf8", function (
-    err,
-    data
-  ) {
-    if (err) throw err;
-    obj = JSON.parse(data);
-  });
-  res.send(obj);
+  res.json({ msg: "Hellos" });
 };
