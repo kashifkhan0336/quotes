3830e81b55414799a03bca3bd2f46eae19ddf0e2
kashifkhan0336 <kashifkhan0336@gmail.com>
2021-01-14 17:37:26 -0800

    w

--- api/quote.js
+++ api/quote.js
@@ -2,5 +2,9 @@ var fs = require("fs");
 var path = require("path");
 
 module.exports = (req, res) => {
-  res.json({ msg: "Hellos" });
+  const file = fs.readFileSync(
+    path.join(__dirname, "_files", "quotes.json"),
+    "utf8"
+  );
+  res.json({ msg: file });
 };
