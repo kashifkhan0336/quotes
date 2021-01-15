329e1290c1d317a79c86577afcf93c2439f85058
kashifkhan0336 <kashifkhan0336@gmail.com>
2021-01-14 17:28:39 -0800

    lol

--- api/quote.js
+++ api/quote.js
@@ -1,13 +1,14 @@
 var fs = require("fs");
 var path = require("path");
-var obj;
-fs.readFile(path.join(__dirname, "_files", "quotes.json"), "utf8", function (
-  err,
-  data
-) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-});
+
 module.exports = (req, res) => {
+  var obj;
+  fs.readFile(path.join(__dirname, "_files", "quotes.json"), "utf8", function (
+    err,
+    data
+  ) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+  });
   res.send(obj.quotes[Math.floor(Math.random() * obj.quotes.length)]);
 };
